166eea82e3c64d4116f2182f4f42fc8c022d5f80
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-05-12 16:28:29 +0000

    Updated webclient configuration

--- beakon/src/main/java/com/newyeti/apiscraper/adapter/beakon/config/WebClientConfig.java
+++ beakon/src/main/java/com/newyeti/apiscraper/adapter/beakon/config/WebClientConfig.java
@@ -3,17 +3,15 @@ package com.newyeti.apiscraper.adapter.beakon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class WebClientConfig {
     
     @Bean
-    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient
                 .builder()
-                .baseUrl("https://v3.football.api-sports.io");
+                .baseUrl("https://api-football-v1.p.rapidapi.com/v3");
     }
 
 }
