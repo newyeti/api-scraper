9ba47ad4d237e1436d33d60b7271b188691db660
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-05-13 19:34:11 +0000

    Implemented WebClient

--- beakon/src/main/java/com/newyeti/apiscraper/adapter/beakon/rest/standings/LeagueStandingsController.java
+++ beakon/src/main/java/com/newyeti/apiscraper/adapter/beakon/rest/standings/LeagueStandingsController.java
@@ -1,22 +1,25 @@
 package com.newyeti.apiscraper.adapter.beakon.rest.standings;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import com.newyeti.apiscraper.adapter.beakon.rest.standings.dto.LeagueDto;
+
+import com.newyeti.apiscraper.adapter.beakon.config.WebClientConfig;
+import com.newyeti.apiscraper.adapter.beakon.rest.http.HttpClient;
+import com.newyeti.apiscraper.adapter.beakon.rest.standings.dto.RequestDto;
+import com.newyeti.apiscraper.adapter.beakon.rest.standings.dto.ResponseDto;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.newyeti.apiscraper.adapter.beakon.rest.standings.mapper.LeagueStandingsMapper.LEAGUE_STANDING_MAPPER;
+//import static com.newyeti.apiscraper.adapter.beakon.rest.standings.mapper.LeagueStandingsMapper.LEAGUE_STANDING_MAPPER;
 
 @Tag(name="standings", description = "Pull data from api and put in a kafka topic")
 @RestController
@@ -25,24 +28,25 @@ import static com.newyeti.apiscraper.adapter.beakon.rest.standings.mapper.League
 @Slf4j
 public class LeagueStandingsController {
     
-    private final WebClient.Builder webClientBuilder;
+    private final HttpClient httpClient;
 
-    @PostMapping("/{season}/{league}")
+    @PostMapping(value = "/pull", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public void pullData(@RequestHeader MultiValueMap<String, String> httpHeaders, @PathVariable String season, @PathVariable String league) {
-        String result = webClientBuilder.build()
-            .get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/standings")
-                .queryParam("season", season)
-                .queryParam("league", league)
-                .build())
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
+    public ResponseDto pullData(@Valid @RequestBody RequestDto requestDto) {
         
+        String result = httpClient
+            .get(uriBuilder -> uriBuilder
+            .path("/standings")
+            .queryParam("season", requestDto.getSeason())
+            .queryParam("league", requestDto.getLeague())
+            .build(), String.class)
+            .block();
+            
         log.info(result);
         // System.out.println(LEAGUE_STANDING_MAPPER.toDomain(leagueDto));
+        return ResponseDto.builder()
+            .status("success")
+            .build();
     }
 
 }
