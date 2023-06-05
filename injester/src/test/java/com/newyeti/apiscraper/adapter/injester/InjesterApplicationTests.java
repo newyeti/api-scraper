87bf8895182107aa9dc092f26482809e5d0d2809
sachindra maharjan <ca77.sam@gmail.com>
2023-06-05 23:10:35 +0000

    Added Injester service module

---
+++ injester/src/test/java/com/newyeti/apiscraper/adapter/injester/InjesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newyeti.apiscraper.adapter.injester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
