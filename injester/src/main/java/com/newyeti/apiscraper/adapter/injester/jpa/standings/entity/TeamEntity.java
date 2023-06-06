1c8c0177e7bc00335ff18a8be8d75446a99bd5e9
sachindra maharjan <ca77.sam@gmail.com>
2023-06-06 18:08:25 +0000

    Added map struct for entity mapping

---
+++ injester/src/main/java/com/newyeti/apiscraper/adapter/injester/jpa/standings/entity/TeamEntity.java
@@ -0,0 +1,16 @@
+package com.newyeti.apiscraper.adapter.injester.jpa.standings.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamEntity {
+    int id;
+    String name;
+    String logo;
+}
--- injester/src/main/java/com/newyeti/apiscraper/adapter/injester/jpa/standings/mapper/LeagueStandingsJpaMapper.java
+++ injester/src/main/java/com/newyeti/apiscraper/adapter/injester/jpa/standings/mapper/LeagueStandingsJpaMapper.java
@@ -1,8 +1,15 @@
 package com.newyeti.apiscraper.adapter.injester.jpa.standings.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
-public class LeagueStandingsJpaMapper {
+import com.newyeti.apiscraper.adapter.injester.jpa.standings.entity.LeagueStandingsEntity;
+import com.newyeti.apiscraper.domain.model.avro.schema.League;
+
+@Mapper(config = StructMapperConfig.class)
+public interface LeagueStandingsJpaMapper {
     
+    @Mapping(target = "leagueId", source = "id")
+    LeagueStandingsEntity toLeagueStandingsEntity(League league);
+
 }
