de0f997471d7ac3dd7198b4eae49d75a3cd39e37
sachindra maharjan <ca77.sam@gmail.com>
2023-06-07 17:09:10 +0000

    Added common module

---
+++ common/src/main/java/com/newyeti/apiscraper/common/exception/AvroException.java
@@ -0,0 +1,21 @@
+package com.newyeti.apiscraper.common.exception;
+
+import org.apache.kafka.common.KafkaException;
+
+public class AvroException extends KafkaException{
+    public AvroException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AvroException(String message) {
+        super(message);
+    }
+
+    public AvroException(Throwable cause) {
+        super(cause);
+    }
+
+    public AvroException() {
+        super();
+    }
+}
