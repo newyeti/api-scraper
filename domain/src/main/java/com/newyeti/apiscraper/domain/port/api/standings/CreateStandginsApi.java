a900ad9e3be9cd819fcef40bca4aedfcba8c17d9
sachindra maharjan <ca77.sam@gmail.com>
2023-06-08 13:30:03 +0000

    Fixed compile error.

--- domain/src/main/java/com/newyeti/apiscraper/domain/port/api/standings/CreateStandginsApi.java
+++ domain/src/main/java/com/newyeti/apiscraper/domain/port/api/standings/CreateStandginsApi.java
@@ -1,7 +1,8 @@
 package com.newyeti.apiscraper.domain.port.api.standings;
 
+import com.newyeti.apiscraper.common.exception.ServiceException;
 import com.newyeti.apiscraper.domain.model.avro.schema.League;
 
 public interface CreateStandginsApi {
-    void save(League league);
+    void save(League league) throws ServiceException;
 }
--- domain/src/main/java/com/newyeti/apiscraper/domain/port/api/standings/GetStandingsApi.java
+++ domain/src/main/java/com/newyeti/apiscraper/domain/port/api/standings/GetStandingsApi.java
@@ -1,7 +1,8 @@
 package com.newyeti.apiscraper.domain.port.api.standings;
 
+import com.newyeti.apiscraper.common.exception.ServiceException;
 import com.newyeti.apiscraper.domain.model.avro.schema.League;
 
 public interface GetStandingsApi {
-    League findByLeagueIdAndSeason(int leagueId, int season);
+    League findByLeagueIdAndSeason(int leagueId, int season) throws ServiceException;
 }
