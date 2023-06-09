2ca8ca817224a68541c75b8893c912fcd5bae785
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-06-09 08:24:27 -0400

    Refactoring

--- application/src/main/java/com/newyeti/apiscraper/application/Application.java
+++ application/src/main/java/com/newyeti/apiscraper/application/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.newyeti.apisraper.*"})
+@ComponentScan({"com.newyeti.apiscraper.*"})
 public class Application {
 
 	public static void main(String[] args) {
