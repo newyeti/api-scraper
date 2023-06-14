393c6863b2c87e53c3e1a7ff7977b2d8bddf822e
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-06-14 13:10:56 +0000

    Updated tests

--- infrastructure/src/main/java/com/newyeti/apiscraper/infrastructure/jpa/mongo/CreateStandingsJpaAdapter.java
+++ infrastructure/src/main/java/com/newyeti/apiscraper/infrastructure/jpa/mongo/CreateStandingsJpaAdapter.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import com.newyeti.apiscraper.domain.model.avro.schema.League;
 import com.newyeti.apiscraper.domain.port.spi.standings.CreateStandingsJpaPort;
+import com.newyeti.apiscraper.infrastructure.jpa.mongo.standings.repository.StandingsRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
