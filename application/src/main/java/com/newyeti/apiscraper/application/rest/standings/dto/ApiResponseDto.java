9b31632c16ba3110fbc0a0797c078bb328274a0d
Sachindra Maharjan <sachindra.maharjan4@gmail.com>
2023-06-20 16:03:15 +0000

    Removed _class from document.

--- application/src/main/java/com/newyeti/apiscraper/application/rest/standings/dto/ApiResponseDto.java
+++ application/src/main/java/com/newyeti/apiscraper/application/rest/standings/dto/ApiResponseDto.java
@@ -3,6 +3,8 @@ package com.newyeti.apiscraper.application.rest.standings.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown=true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponseDto {
     
     private String get;
@@ -36,7 +39,9 @@ public class ApiResponseDto {
     @NoArgsConstructor
     @AllArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown=true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Response {
+        @JsonProperty("league")
         private LeagueStandingsDto leagueStandingsDto;  
     }
 
