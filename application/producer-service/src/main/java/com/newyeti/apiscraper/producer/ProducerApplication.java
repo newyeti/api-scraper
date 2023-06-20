518f478b2c9f44261c5fd77f61d654e07c3772d5
Sachindra Maharjan <sachindra.maharjan4@gmail.com>
2023-06-20 21:54:53 +0000

    Fixed build issue

--- application/producer-service/src/main/java/com/newyeti/apiscraper/producer/ProducerApplication.java
+++ application/producer-service/src/main/java/com/newyeti/apiscraper/producer/ProducerApplication.java
@@ -2,8 +2,14 @@ package com.newyeti.apiscraper.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
 
 @SpringBootApplication(scanBasePackages = "com.newyeti.apiscraper")
+@ComponentScan(basePackages = {"com.newyeti.apiscraper"},
+	excludeFilters = @Filter(type=FilterType.ASPECTJ, 
+		pattern = "com.newyeti.apiscraper.infrastructure.jpa.*"))
 public class ProducerApplication {
 
 	public static void main(String[] args) {
